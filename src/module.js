55ca2971b16d7b83ba711d2eb87e6a7c3a99364d
VitaliiBo <boflying1@gmail.com>
2020-06-01 18:01:12 +0300

    "second commit"

--- src/module.js
+++ src/module.js
@@ -3,26 +3,51 @@ function Character(name, type) {
         health: 100,
         level: 1,
         type: {
-           'Bowman': [ 25, 25 ],
-           'Swordsman': [ 40, 10 ],
-           'Magician': [ 10, 40],
-           'Undead': [ 25, 25 ],
-           'Zombie': [ 40, 10 ],
-           'Daemon': [ 10, 40 ],
+           'Bowman': {
+               attack: 25,
+               defence: 25,
+           },
+           'Swordsman': {
+                attack: 25,
+                defence: 25,
+            },
+           'Magician': {
+                attack: 25,
+                defence: 25,
+             },
+           'Undead': {
+                attack: 25,
+                defence: 25,
+            },
+           'Zombie': {
+                attack: 25,
+                defence: 25,
+            },
+           'Daemon': {
+                attack: 25,
+                defence: 25,
+            },
         },
     }
     try {
         if ( name.length < 2 || name.length > 10) {
             throw new Error('Имя должно быть длиннее 2х и меньше 10ти символов');
         }
-
+        const types = Object.prototype.defaultCharacter.type
+        this.name = name;
+        this.type = type;
+        for (const item in types) {
+            if ( type === item ) {    
+                 this.attack = types[item].attack;
+                 this.defence = types[item].defence;                
+            }
+        }
     } catch (error) {
         console.log(error);
     }
 
-    this.name = name;
-    this.type = type;
+    
   }
 
-  console.log(new Character('B', 'Daemon'));
+  console.log(new Character('R2D2', 'Daemon'));
   
